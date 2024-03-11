fe9f643291d907aec1dc3720206e883164321aa1
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-03-11 15:24:22 +0500

    add folders

---
+++ Medium.Application/Abstraction/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Medium.Domen.Entitys;
+using Microsoft.EntityFrameworkCore;
+
+namespace Medium.Application.Abstraction
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<User> Users { get; set; }
+        Task SaveChangesAsync(CancellationToken cancellation);
+    }
+}
---
+++ Medium.Domen/Entitys/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Medium.Domen.Entitys
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Bio { get; set; }
+        public string PhotoPath { get; set; }
+        public int Followers { get; set; }
+
+        public string Login {  get; set; }
+        public string PasswordHash { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public DateTime JoinDate { get; set; }
+        public DateTime ModifiedDate { get; set;}
+        public DateTime DateTime {  get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
